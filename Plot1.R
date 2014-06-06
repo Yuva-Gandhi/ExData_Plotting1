c27dae8103210fa52f0cc52175b3975c1b6e8e96
yuva <yuvamathi@gmail.com>
2014-06-06 13:02:09 -0700

    updated comments

--- Plot1.R
+++ Plot1.R
@@ -1,3 +1,4 @@
+# Data pre processing to extract required dates and convert to date time classes
 data <- read.table("household_power_consumption.txt",sep=";", header=TRUE)
 data$Time<-as.character(data$Time)
 data$Date<-as.character(data$Date)
@@ -7,7 +8,7 @@ temp <- paste(x$Date,x$Time)
 x$Datetime <- strptime(temp, format="%Y-%m-%d %H:%M:%S")
 
 png("Plot1.png")
-
+# Draw histogram
 hist((as.numeric(x$Global_active_power))/500,col = "red", xlab = "Global Active Power (kilowatts)", 
      main = "Global Active Power",cex.lab=.75, cex.axis=.75, cex.main=.75, cex.sub=.75)
 par(mar = c(3,2,3,3))
